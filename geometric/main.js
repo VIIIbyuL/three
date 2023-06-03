9eb31e1d8e1580bc33ca43573d89c9f86b98f17d
VIIIbyuL <myjjk03@gmail.com>
2023-06-03 19:25:31 -0400

    took so mf long but stars are now clickable

--- geometric/main.js
+++ geometric/main.js
@@ -17,7 +17,7 @@ scene.background = spaceText;
 renderer.setPixelRatio( window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight);
 
-camera.position.set(0,0,120);
+camera.position.set(0,0,0);
 
 renderer.render(scene, camera);
 
@@ -57,8 +57,46 @@ var objectiveInfo;
 var objectiveNum = 0;
 const numArray = [];
 
+
 document.getElementById("submitbtn").addEventListener("click", handleSubmit);
 document.getElementById("removeRec").addEventListener("click", handleRem);
+
+// holy motherrrrrrrrr this code was so harddddduhhhhh
+document.addEventListener('mousedown', onDocumentMouseDown, false);
+const raycaster = new THREE.Raycaster();
+
+function onDocumentMouseDown(event) {
+  const mouse = new THREE.Vector2();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  const formElement = document.getElementById("form");
+  const rect = formElement.getBoundingClientRect();
+
+  // Check if the mouse coordinates intersect with the form element's bounding box
+  if (
+    mouse.x >= rect.left &&
+    mouse.x <= rect.right &&
+    mouse.y >= rect.top &&
+    mouse.y <= rect.bottom
+  ) {
+    return; // Ignore clicks on the form
+  }
+
+  raycaster.setFromCamera(mouse, camera);
+
+  const intersects = raycaster.intersectObjects(scene.children);
+
+  if (intersects.length > 0) {
+    const clickedObject = intersects[0].object;
+
+    if (clickedObject instanceof THREE.Mesh && clickedObject.userData.star !== undefined) {
+      const starNum = clickedObject.userData.star;
+
+      console.log('Star clicked:', starNum);
+    }
+  }
+}
 
 function handleRem() {
   if (numArray.length === 0) {
@@ -80,6 +118,8 @@ function handleRem() {
   // Remove the star if found
   if (starToRemove) {
     scene.remove(starToRemove);
+   
+  
     console.log("Removal of star with value " + starNum + " success");
   } else {
     console.log("Star with value " + starNum + " not found");
@@ -117,14 +157,16 @@ function addStar(userStar) {
     emissiveIntensity: 0.2, // Adjust the intensity as desired
   });
   const star = new THREE.Mesh(geometry, material);
-  star.userData.value = userStar.objectiveNum;
+  star.userData.star = userStar;
 
   const [x, y, z] = Array(3)
     .fill()
     .map(() => THREE.MathUtils.randFloatSpread(100));
 
   star.position.set(x, y, z);
-  console.log(`star is ${userStar.objective} and ${star.userData.value}`);
+  console.log(`star is ${userStar.objective} and ${star.userData.star}`);
+
+
   scene.add(star);
 }
 
@@ -133,6 +175,7 @@ function addStar(userStar) {
 
 function animate() {
   renderer.render( scene, camera);
+
   requestAnimationFrame( animate );
 }
 
